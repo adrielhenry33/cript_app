5c1e454d5cc58867304062a2f58111f70bfc9270
adrielhenry33 <adrielhenrydb@gmail.com>
2025-06-11 10:16:25 -0300

    fix: itens centralized

--- flutter_aula_1/lib/pages/moedas_detalhes_page.dart
+++ flutter_aula_1/lib/pages/moedas_detalhes_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_aula_1/models/moeda.dart';
+import 'package:intl/intl.dart';
 
-class MoedasDetalhesPage extends StatefulWidget{
-
+class MoedasDetalhesPage extends StatefulWidget {
   final Moeda moeda;
 
   const MoedasDetalhesPage({super.key, required this.moeda});
@@ -12,11 +12,38 @@ class MoedasDetalhesPage extends StatefulWidget{
 }
 
 class _MoedasDetalhesPage extends State<MoedasDetalhesPage> {
-  @override 
-  Widget build(BuildContext context){
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.moeda.nome ),
+      appBar: AppBar(title: Text(widget.moeda.nome)),
+      body: Padding(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(width: 50, child: Image.asset(widget.moeda.icone)),
+                  Container(width: 10),
+                  Text(
+                    real.format(widget.moeda.preco),
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -1,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
